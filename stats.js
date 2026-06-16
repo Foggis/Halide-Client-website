8473c2d009b8f09c045e00328fe6c319fb0789ed
Fogma <107138510+Foggis@users.noreply.github.com>
2026-06-16 04:55:42 +0200

    Removed stats.js, fixed easyily misinterpreted text

--- stats.js
+++
@@ -1,6 +0,0 @@
-fetch("stats.json")
-.then(r => r.json())
-.then(s => {
-  document.getElementById("playtime").innerText = s.totalPlaytime;
-  document.getElementById("launches").innerText = s.totalLaunches;
-});
